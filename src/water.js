d3478867a71bea8039316a8738c644f3113443b2
Jan Hug <dulnan@me.com>
2015-09-18 22:05:36 +0200

    working setup and code

--- src/water.js
+++ src/water.js
@@ -4,19 +4,18 @@ var ms = "ms";
 var sinus = math.sin;
 onload = function() {
     for (j=15; j>0; j--) {
-    var row = doc.createElement('p');
+        var row = doc.createElement('p');
 
-    for (i=50; i>0; i--){
-        var cell = doc.createElement('i');
-        var cellStyle = cell.style;
+        for (i=50; i>0; i--){
+            var cell = doc.createElement('i');
+            var cellStyle = cell.style;
+            var cellAnimationDelay = sinus(i*math.random()*5)*i*(20*j)-9999+ms;
+            var cellAnimationDuration = ((sinus(j*i/5))+800)*math.sqrt(j/2)+ms;
 
-        var cellAnimationDelay = sinus(i*math.random()*5)*i*(20*j)-9999+ms;
-        var cellAnimationDuration = ((sinus(j*i/5))+800)*math.sqrt(j/2)+ms;
+            cellStyle.webkitAnimationDelay = cellAnimationDelay;
+            cellStyle.webkitAnimationDuration = cellAnimationDuration;
 
-        cellStyle.webkitAnimationDelay = cellAnimationDelay;
-        cellStyle.webkitAnimationDuration = cellAnimationDuration;
-
-        row.appendChild(cell);
-    };
+            row.appendChild(cell);
+        };
     doc.body.appendChild(row);
 }}
